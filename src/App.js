0f5ae7de6318e9b0609d0f1276d958c8ecdd88f7
Deepakjoseos <93700189+Deepakjoseos@users.noreply.github.com>
2022-07-16 21:29:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Products from "./Products";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Products />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./styles.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>E-commerce App</h1>
+    </div>
+  );
+}
